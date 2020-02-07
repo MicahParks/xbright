95f10c3c28845ce56d62cbf22a4c19783ad27700
Micah Parks <micahleviparks@gmail.com>
2020-02-07 16:03:39 -0500

    Map displays to brightness.

--- xrandr.go
+++ xrandr.go
@@ -6,12 +6,14 @@ import (
 	"io/ioutil"
 	"os/exec"
 	"regexp"
+	"strconv"
 )
 
 var noDisplays = errors.New("no connected displays found in xrandr --query")
+var notDisplay = errors.New("given string is not a connected display")
 
 type xrandr struct {
-	displays []string
+	displays map[string]float64
 }
 
 func main() {
@@ -25,7 +27,7 @@ func main() {
 }
 
 func (x *xrandr) getDisplays() error {
-	cmd := exec.Command("xrandr", "--query")
+	cmd := exec.Command("xrandr", "--verbose", "--query")
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
 		return err
@@ -45,9 +47,29 @@ func (x *xrandr) getDisplays() error {
 	if bDisplays == nil {
 		return noDisplays
 	}
-	x.displays = make([]string, 0)
+	displays := make([]string, 0)
 	for _, bDisplay := range bDisplays {
-		x.displays = append(x.displays, string(bytes.Split(bDisplay, []byte(" "))[0]))
+		displays = append(displays, string(bytes.Split(bDisplay, []byte(" "))[0]))
+	}
+	re = regexp.MustCompile(`Brightness.*`)
+	bBrightnesses := re.FindAll(out, -1)
+	if bBrightnesses == nil {
+		// TODO Error
+	}
+	brightnesses := make([]float64, 0)
+	for _, bBrightness := range bBrightnesses {
+		bFloat := bytes.Split(bBrightness, []byte(" "))[1]
+		float, err := strconv.ParseFloat(string(bFloat), 64)
+		if err != nil {
+			return err
+		}
+		brightnesses = append(brightnesses, float)
+	}
+	if len(brightnesses) != len(displays) {
+		// TODO Error
+	}
+	for i, display := range displays {
+		x.displays[display] = brightnesses[i]
 	}
 	return nil
 }
@@ -60,5 +82,20 @@ func (x *xrandr) new() error {
 	if err != nil {
 		return err
 	}
+	return nil
+}
+
+func (x *xrandr) setBrightness(set string, val float64) error {
+	good := false
+	for _, display := range x.displays {
+		if display == set {
+			good = true
+			break
+		}
+	}
+	if !good {
+		return notDisplay
+	}
+
 	return nil
 }
