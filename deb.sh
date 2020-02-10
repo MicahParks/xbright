89a272830adc110d9ceea6fe8661552e20ac773b
Micah Parks <micahleviparks@gmail.com>
2020-02-10 12:33:35 -0500

    conflicting name with binary.

--- deb.sh
+++ deb.sh
@@ -2,9 +2,9 @@
 
 $GOPATH/bin/fyne package -os linux -icon icon.png
 tar xzf xbright.tar.gz
-mkdir xbright
-mv usr xbright/
-mkdir xbright/DEBIAN
+mkdir xbright_1.0
+mv usr xbright_1.0/
+mkdir xbright_1.0/DEBIAN
 echo \
 'Package: xbright
 Version: 1.0-1
@@ -13,6 +13,6 @@ Architecture: amd64
 Maintainer: Micah Parks <micahleviparks@gmail.com>
 Description: xbright
  A simple brightness GUI for xrandr.' \
-> xbright/DEBIAN/control
+> xbright_1.0/DEBIAN/control
 # Use dpkg to build and publish.
-dpkg-deb --build xbright/
+dpkg-deb --build xbright_1.0/
